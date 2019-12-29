2d7eb9464f13d909b97a4d16315b6dced24705b0
Brandon Weaver <a7xncob@gmail.com>
2019-12-29 00:02:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,14 +4,13 @@ class App < Sinatra::Base
   get '/reverse' do
     erb :reverse
   end
-
+  
   post '/reverse' do
-    params[:string].reverse
+    @reversed_string = params[:string].reverse
     erb :reversed
   end
-
+  
   get '/friends' do
     # Write your code here!
-
   end
 end
